96be0d1dfda493531ead6f2c3085f220cc32eca9
Felipe Delfino <fejobs9@gmail.com>
2026-09-10 22:37:34 +0100

    updated

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ app.use(express.json()) // para que o arquivo entenda que esta recebendo arquivo
 const database = [] //'banco de dados'
 
 app.post('/', (req, res) => {
- 
-  database.push (req.body)  //rota de envio de dados
-  res.status(201)     //status 201 td ok e foi criada sua  requisicao
+   database.push (req.body)  //rota de envio de dados
+   res.status(201).json(req.body)    //status 201 td ok e foi criada sua  requisicao
 })
 
 app.get('/', (req, res) => {  //rota de recebimento de dados 
-  res.status(200).json(database)  //status (200) 'td ok'      
+   res.json(database)
+   res.status(200).json(database)  //status (200) 'td ok'      
 });
+app.delete('/' , (req,res) => {
+  res.json(database)
+})
 
-app.listen(5500);
+app.listen(51620)
